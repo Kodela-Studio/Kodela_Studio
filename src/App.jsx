ef3da497b97c57c09df459e855605822b402e0b0
helena-cruse <helena.m.cruse@gmail.com>
2026-06-11 16:31:04 +0200

    Add about section with team profiles

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,19 @@
 import Header from "./components/Header";
 import Hero from "./components/Hero";
-import Footer from "./components/Footer";
 import Services from "./components/Services";
+import About from "./components/About";
 import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="min-h-screen bg-kodela-black text-kodela-cream">
       <Header />
       <Hero />
-      <Footer />
       <Services />
+      <About />
       <Projects />
+      <Footer />
     </div>
   );
 }
