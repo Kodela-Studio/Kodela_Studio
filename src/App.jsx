b35325b77af1d733f6ba7a7d0adc222292dc5296
Helena <helena.m.cruse@gmail.com>
2026-09-16 13:29:13 +0200

    Route contact page

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,19 @@ import Services from "./components/Services";
 import About from "./components/About";
 import Footer from "./components/Footer";
 import Packages from "./components/Packages";
+import Contact from "./components/Contact";
 import Chatbot from "./components/Chatbot";
 
 function App() {
   const pathname = window.location.pathname.toLowerCase().replace(/\/$/, "");
   const isPackagesPage = pathname === "/packages";
   const isAboutPage = pathname === "/om-oss";
+  const isContactPage = pathname === "/kontakt";
 
   return (
     <div className="min-h-screen bg-kodela-black text-kodela-cream">
       <Header />
-      {isPackagesPage ? <Packages /> : isAboutPage ? <About page /> : <><Hero /><Services /><About /></>}
+      {isPackagesPage ? <Packages /> : isAboutPage ? <About page /> : isContactPage ? <Contact /> : <><Hero /><Services /><About /></>}
       <Footer />
       <Chatbot />
     </div>
