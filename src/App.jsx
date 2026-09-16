8d09edebfa8ff84b6d92d70176edea9fd80e6a85
Helena <helena.m.cruse@gmail.com>
2026-09-16 12:27:10 +0200

    Remove standalone projects section

--- src/App.jsx
+++ src/App.jsx
@@ -2,31 +2,18 @@ import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Services from "./components/Services";
 import About from "./components/About";
-import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import Packages from "./components/Packages";
 import Chatbot from "./components/Chatbot";
 
 function App() {
   const pathname = window.location.pathname.toLowerCase().replace(/\/$/, "");
-
   const isPackagesPage = pathname === "/packages";
 
   return (
     <div className="min-h-screen bg-kodela-black text-kodela-cream">
       <Header />
-
-      {isPackagesPage ? (
-        <Packages />
-      ) : (
-        <>
-          <Hero />
-          <Services />
-          <About />
-          <Projects />
-        </>
-      )}
-
+      {isPackagesPage ? <Packages /> : <><Hero /><Services /><About /></>}
       <Footer />
       <Chatbot />
     </div>
